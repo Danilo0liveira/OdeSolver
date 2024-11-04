bdc155060f5afe6a572bebe88a1f373d11772969
Danilo0liveira <danilo.melo@ee.ufcg.edu.br>
2024-11-04 12:40:08 -0300

    ModifiedRungeKuttaFourthOrder input modified

--- OdeSolver/Program.cs
+++ OdeSolver/Program.cs
@@ -15,38 +15,70 @@ class Program
     static void Main()
     {
         double t = 0;
-        double ta = 0.5;
+        double ta = 5;
         double tf = 600;
         int it = 0;
         double array_size = (tf-t)/ta;
         Vector<double> y;
 
-        double[] results = new double[(int)array_size + 1];
-        double[] tempos = new double[(int)array_size + 1];
+        double[] y0 = new double[(int)array_size + 1];
+        double[] y1 = new double[(int)array_size + 1];
+        double[] y2 = new double[(int)array_size + 1];
+        double[] y3 = new double[(int)array_size + 1];
+        double[] times = new double[(int)array_size + 1];
         
         var u = Vector<double>.Build.Dense(new double[] { 16.6, 0.55, 15.6, 0 });
         var x0 = Vector<double>.Build.Dense(new double[] { 14, -4.32e-4, 5.28e-4, 0 });
+        var y_in = x0;
 
         while (t <= tf)
         {
+            // step at t = 200 s
             if (t == 200.0)
-                u[2] = u[2]*0.90;
+                u[2] = u[2]*1.05;
 
-            y = phModel(u: u, x0: x0, t: t, ta: ta);
+            y = phModel(u: u, x0: x0, y_in : y_in, t: t, ta: ta);
 
-            results[it] = y[3];
-            tempos[it] = t;
+            //plot vectors
+            y0[it] = y[0];
+            y1[it] = y[1];
+            y2[it] = y[2];
+            y3[it] = y[3];
+            times[it] = t;
 
+            // simulation update
             t += ta;
             it += 1;
+            y_in = y;
         }
 
-        ScottPlot.Plot myPlot = new();
+        ScottPlot.Multiplot multiplot = new();
 
-        myPlot.Add.Scatter(tempos, results);
-        myPlot.SavePng("ph1.1.png", 400, 300);
+        ScottPlot.Plot plot1 = new();
+        ScottPlot.Plot plot2 = new();
+        ScottPlot.Plot plot3 = new();
+        ScottPlot.Plot plot4 = new();
+
+        plot1.Add.Scatter(times, y0);
+        plot2.Add.Scatter(times, y1);
+        plot3.Add.Scatter(times, y2);
+        plot4.Add.Scatter(times, y3);
+
+        plot1.Title("h");
+        plot2.Title("wa4");
+        plot3.Title("wb4");
+        plot4.Title("pH");
+
+        multiplot.AddPlot(plot1);
+        multiplot.AddPlot(plot2);
+        multiplot.AddPlot(plot3);
+        multiplot.AddPlot(plot4);
+
+        multiplot.Layout = new ScottPlot.MultiplotLayouts.Columns();
+
+        multiplot.SavePng("q3_delta05.png", 800, 600);
     }
-    static Vector<double> phModel(Vector<double> u, Vector<double> x0, double t, double ta)
+    static Vector<double> phModel(Vector<double> u, Vector<double> x0, Vector<double> y_in, double t, double ta)
     {
         double pk1 = 6.35;
         double pk2 = 10.25;
@@ -54,7 +86,7 @@ class Program
         double q2 = u[1];
         double q3 = u[2];
 
-        var ode_out = ModifiedRungeKuttaFourthOrder(x0, 0, t+ta, N: 100, (t, y) => dxdt(t, y, q1, q2, q3));
+        var ode_out = ModifiedRungeKuttaFourthOrder(y_in, t, t+ta, N: 100, (t, y_in) => dxdt(t, y_in, q1, q2, q3));
 
         Vector<double> x = Vector<double>.Build.Dense(new double[] {
             ode_out[ode_out.Length - 1][0],
@@ -76,7 +108,7 @@ class Program
 
         return y;
     }
-    public static Vector<double>[] ModifiedRungeKuttaFourthOrder(Vector<double> y, double start, double end, int N, Func<double, Vector<double>, Vector<double>> f)
+    public static Vector<double>[] ModifiedRungeKuttaFourthOrder(Vector<double> y_in, double start, double end, int N, Func<double, Vector<double>, Vector<double>> f)
     {
         double num = (end - start) / (double)(N - 1);
         double num2 = start;
@@ -84,14 +116,14 @@ class Program
 
         for (int i = 0; i < N; i++)
         {
-            Vector<double> num3 = f(num2, y);
-            Vector<double> num4 = f(num2 + num / 2.0, y + num3 * num / 2.0);
-            Vector<double> num5 = f(num2 + num / 2.0, y + num4 * num / 2.0);
-            Vector<double> num6 = f(num2 + num, y + num5 * num);
+            Vector<double> num3 = f(num2, y_in);
+            Vector<double> num4 = f(num2 + num / 2.0, y_in + num3 * num / 2.0);
+            Vector<double> num5 = f(num2 + num / 2.0, y_in + num4 * num / 2.0);
+            Vector<double> num6 = f(num2 + num, y_in + num5 * num);
 
-            vectors[i] = y + num / 6.0 * (num3 + 2.0 * num4 + 2.0 * num5 + num6);
+            vectors[i] = y_in + num / 6.0 * (num3 + 2.0 * num4 + 2.0 * num5 + num6);
             num2 += num;
-            y = vectors[i];
+            y_in = vectors[i];
         }
         return vectors;
     }
